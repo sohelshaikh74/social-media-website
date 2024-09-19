75bde62ae6c6a007259c3e872a2c8aac596e29cd
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-19 11:02:56 +0530

    All updated

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const theme = document.querySelector('#theme')
 const themeModal = document.querySelector('.customize-theme')
 const fontSize = document.querySelectorAll('.choose-size span')
 var root = document.querySelector(':root')
+const colorPalette = document.querySelectorAll('.choose-color span')
 
 // REMOVE ACTIVE CLASS FROM ALL MENU ITEMS
 const changeActiveItem = ()=>{
@@ -127,6 +128,37 @@ fontSize.forEach((size)=>{
         document.querySelector('html').style.fontSize =fontSize
      })
 
+})
+
+// remove active class from colors
+const changeActiveColorClass = ()=>{
+    colorPalette.forEach((colorPicker)=>{
+        colorPicker.classList.remove('active')
+    })
+}
+// change primary color
+colorPalette.forEach((color)=>{
+    color.addEventListener('click',()=>{
+        let primary;
+        changeActiveColorClass()
+        if(color.classList.contains('color-1')){
+            primaryHue=252;
+        }
+        else if(color.classList.contains('color-2')){
+            primaryHue=52;
+        }
+        else if(color.classList.contains('color-3')){
+            primaryHue=352
+        }
+        else if(color.classList.contains('color-4')){
+            primaryHue=152
+        }
+        else if(color.classList.contains('color-5')){
+            primaryHue=202
+        }
+        color.classList.add('active')
+        root.style.setProperty('--primary-color-hue',primaryHue)
+    })
 })
 
 
